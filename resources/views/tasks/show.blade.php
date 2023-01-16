0e6858983b3b699af5392a3bead00c2f4e271ce8
kazuki.sano <teq29pro80h@gmail.com>
2023-01-16 14:12:16 +0000

    CRUD messages

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -20,4 +20,13 @@
     
     <a class="btn btn-outline" href="{{ route('tasks.edit', $task->id) }}">このタスクを編集</a>
     
+     {{-- メッセージ削除フォーム --}}
+    <form method="POST" action="{{ route('tasks.destroy', $task->id) }}" class="my-2">
+        @csrf
+        @method('DELETE')
+        
+        <button type="submit" class="btn btn-error btn-outline" 
+            onclick="return confirm('id = {{ $task->id }} のタスクを削除します。よろしいですか？')">削除</button>
+    </form>
+    
 @endsection
